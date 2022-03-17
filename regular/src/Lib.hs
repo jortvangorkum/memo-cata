18053820b1a413ce12416f22c260bcd46f562cab
Jort van Gorkum <jortvangorkum@gmail.com>
2022-03-17 16:27:30 +0100

    Got here

--- regular/src/Lib.hs
+++ regular/src/Lib.hs
@@ -1,6 +1,68 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import           Generics.Data.Digest.CRC32
+import           Generics.Regular
+import           Generics.Regular.Functions
+import qualified Generics.Regular.Functions.Read as G
+import qualified Generics.Regular.Functions.Show as G
+import           Generics.Regular.TH
+
+data Tree = Leaf Int
+          | Node Tree Int Tree
+          deriving (Show)
+
+$(deriveAll ''Tree "PFTree")
+type instance PF Tree = PFTree
+
+t :: Tree
+t = Node (Leaf 1) 2 (Leaf 3)
+
+type family Merkle (f :: (* -> *)) (h :: *) :: (* -> *)
+
+type instance Merkle (K a)     h = K a :*: K h
+type instance Merkle I         h = I :*: K h
+type instance Merkle (f :+: g) h = (Merkle f h :+: Merkle g h) :*: K h
+type instance Merkle (f :*: g) h = (Merkle f h :*: Merkle g h) :*: K h
+
+class Merkelize f where
+  merkleG :: Merkelize g => f (Fix g) -> (Merkle f Digest) (Fix (Merkle g Digest))
+  -- merkleG :: Merkelize g => f (Fix g) -> (f :*: K Digest) (Fix (g :*: K Digest))
+
+-- merkle :: Merkelize f => Fix f -> Fix (f :*: K Digest)
+-- merkle = In . merkleG . out
+
+instance (Show a) => Merkelize (K a) where
+  merkleG (K x) = K x :*: K h
+    where
+      h = digestConcat [digest "K", digest x]
+
+instance Merkelize I where
+  merkleG (I x) = I prevX :*: K h
+    where
+      prevX@(In (_ :*: K ph)) = In . merkleG . out $ x
+      h = digestConcat [digest "I", ph]
+
+instance (Merkelize f, Merkelize g) => Merkelize (f :+: g) where
+  merkleG (L x) = L prevX :*: K h
+    where
+      (prevX :*: K ph) = merkleG x
+      h = digestConcat [digest "L", ph]
+  merkleG (R x) = R prevX :*: K h
+    where
+      (prevX :*: K ph) = merkleG x
+      h = digestConcat [digest "R", ph]
+
+instance (Merkelize f, Merkelize g) => Merkelize (f :*: g) where
+  merkleG (x :*: y) = (prevX :*: prevY) :*: K h
+    where
+      prevX :*: K phx = merkleG x
+      prevY :*: K phy = merkleG y
+      h = digestConcat [digest "Pair", phx, phy]
